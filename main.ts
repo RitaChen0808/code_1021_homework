c397fa04d8ea2360954a9fb917ec84dd82ab20e8
RitaChen0808 <71751332+RitaChen0808@users.noreply.github.com>
2020-10-28 13:33:36 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
 basic.forever(function () {
-	
+    for (let I = 0; I <= 4; I++) {
+        led.plot(4 - I, 4)
+        led.plot(5 - I, 3)
+        led.plot(6 - I, 2)
+        led.plot(7 - I, 1)
+        led.plot(8 - I, 0)
+        led.plot(4, 4 - I)
+        led.plot(3, 5 - I)
+        led.plot(2, 6 - I)
+        led.plot(1, 7 - I)
+        led.plot(0, 8 - I)
+        basic.pause(100)
+    }
+    basic.clearScreen()
 })
