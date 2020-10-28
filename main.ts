ce3067f1aa44f0dfacae982c68f8c785d61833a9
RitaChen0808 <71751332+RitaChen0808@users.noreply.github.com>
2020-10-28 13:55:42 +0800

    change number

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 basic.forever(function () {
-    for (let I = 0; I <= 4; I++) {
+    for (let I = 0; I <= 8; I++) {
         led.plot(4 - I, 4)
         led.plot(5 - I, 3)
         led.plot(6 - I, 2)
